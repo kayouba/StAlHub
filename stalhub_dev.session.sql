d72f9a955225dfe08839c4e07bb0ca6f809affb6
Kayou Ba <kayou@Host-003.lan>
2025-05-12 15:30:59 +0200

    Seed default user + fix migrations

---
+++ stalhub_dev.session.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (id, email, password, phone_number, created_at)
+VALUES (
+    id:int,
+    'email:varchar',
+    'password:varchar',
+    'phone_number:varchar',
+    'created_at:timestamp'
+  );
