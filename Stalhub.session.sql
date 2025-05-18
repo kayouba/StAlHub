b6c548de5088b3395456c6856f2592786e959158
fsamb <fatimata-binetou.samb@etu.u-bordeaux.fr>
2025-05-18 13:30:50 +0200

    code sql

--- Stalhub.session.sql
+++ Stalhub.session.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (email, password, phone_number, created_at)
 VALUES (
-  'test1@example.com',
+  'test6@example.com',
   '$2y$10$efOBnU0b2L.t/yVAD9TRQuFrAQ05SaKDcRMIWoMN4kugYH0fOPCB2',
   '+33600000000',
   NOW()
--- stalhub_dev.session.sql
+++ stalhub_dev.session.sql
@@ -6,3 +6,15 @@ VALUES (
     'phone_number:varchar',
     'created_at:timestamp'
   );
+
+
+/*ALTER TABLE users
+DROP COLUMN cv_filename;*/
+
+
+
+/*ALTER TABLE users
+ADD COLUMN formation VARCHAR(50),
+ADD COLUMN parcours VARCHAR(50),
+ADD COLUMN  annee VARCHAR(50),
+ADD COLUMN cv_filename VARCHAR(50);*/
